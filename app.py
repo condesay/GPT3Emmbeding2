ee00e49432c9e64a6a358c21d6abdb1dea7996fa
Condesay <39847380+condesay@users.noreply.github.com>
2023-04-03 12:00:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def get_api_key():
     return api_key
 
 # Fonction pour récupérer les embeddings de deux textes en utilisant l'API OpenAI GPT-3
-def get_embeddings(text1, text2, model_id, api_key):
+def get_embeddings(text1, text2, model_engine, api_key):
     openai.api_type = "azure"
     openai.api_key = api_key
     openai.api_base = "https://tsi-openai.openai.azure.com"
@@ -17,7 +17,7 @@ def get_embeddings(text1, text2, model_id, api_key):
 
     embeddings = openai.Embedding.create(
         inputs=[text1, text2],
-        model=model_id,
+        model=model_engine,
         data_output_format="array",
         engine="davinci"
     )["data"]
@@ -30,11 +30,10 @@ def main():
     api_key = get_api_key()
     if api_key:
         model_engine = "de-code-davinci-001"
-        model_id = "text-similarity-davinci-001"
         text1 = st.text_area("Texte 1")
         text2 = st.text_area("Texte 2")
         if st.button("Compare"):
-            embeddings = get_embeddings(text1, text2, model_id, api_key)
+            embeddings = get_embeddings(text1, text2, model_engine, api_key)
             similarity = cosine_similarity(embeddings[0], embeddings[1])
             st.write(f"Le score de similarité de cosinus entre les deux textes est {similarity:.4f}.")
 
