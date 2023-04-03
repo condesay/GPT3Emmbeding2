10f923cef0eaaff6263992a44103994117e5c149
Condesay <39847380+condesay@users.noreply.github.com>
2023-04-03 12:41:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def main():
     st.title("Similarité entre textes")
     api_key = get_api_key()
     if api_key:
-        model_engine = "text-davinci-002"
+        model_engine = "text-embedding-ada-002"
         text1 = st.text_area("Texte 1")
         text2 = st.text_area("Texte 2")
         if st.button("Compare"):
